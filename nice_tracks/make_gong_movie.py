b4e732c30dbcd9d5fe97c37747156b8c31d068bb
jprchlik <japrchlik@gmail.com>
2016-11-16 10:39:33 -0500

    Added function to create new plots based on track id

--- nice_tracks/make_gong_movie.py
+++ nice_tracks/make_gong_movie.py
@@ -1,10 +1,26 @@
 from datetime import datetime, timedelta
+import numpy as np
 import grab_gong
 import add_properties as ap
 import pandas as pd
 from multiprocessing import Pool
 from create_plots import halpha_plot
 import os
+def get_best_tracks(tid):
+
+    track, = np.where(dat['track_id'].values == tid)
+    
+#get the start and end time of the track    
+    start = dat['event_starttime_dt'][track[0]]
+    end   = dat['event_endtime_dt'][track[-1]]
+    #make sure files exist locally and output filenames
+    flist = grab_gong.main(start,end).filelist
+    
+    for i in flist:
+        out = halpha_plot(dat,i,tid,pdir)
+        out.plot_filament_track()
+    
+
 
 #for inital testing
 #fmt = '%Y/%m/%dT%H:%M:%S'
@@ -24,15 +40,9 @@ dat = ap.add_props(dat).dat
 sdir = os.getcwd()
 pdir = sdir+'/track_plots/'
 
+goodtracks = np.loadtxt('list_of_excellent_tracks')
 
-start = dat['event_starttime_dt'][0]
-end   = dat['event_endtime_dt'][2]
-#make sure files exist locally and output filenames
-flist = grab_gong.main(start,end).filelist
-
-for i in flist:
-    out = halpha_plot(dat,i,1,pdir)
-    out.plot_filament_track()
+get_best_track(goodtracks[0])
 
 
 
