49ee6a04386064d7ec8152e18f513f2164df55fd
jprchlik <japrchlik@gmail.com>
2016-11-16 08:54:25 -0500

    Debugged plot procedure. Added pdir to init variables. Removed plot_filament_track from init.

---
+++ nice_tracks/create_plots.py
@@ -0,0 +1,94 @@
+import matplotlib
+matplotlib.rc('font',size=24) #fix multiprocessing issue
+matplotlib.use('agg')
+import shapely 
+import matplotlib.pyplot as plt
+from shapely.wkt  import dumps, loads
+from matplotlib.patches import *
+from sunpy.physics import solar_rotation
+import astropy.units as u
+import pyfits
+import datetime
+
+
+class halpha_plot:
+
+    def __init__(self,dat,ifile,tid,pdir):
+        self.dat = dat
+        self.ifile = ifile
+        self.tid = tid
+
+
+  
+    def  calc_poly_values(self,coor):
+    #list of polygon coordinates
+        corlist =  coor.replace('POLYGON((','').replace('))','').split(',')
+    #create array and calculate mean x,y value for start
+        corarra =  np.zeros((len(corlist),2))
+        for j,k in enumerate(corlist):
+            values = k.split(' ')
+            corarra[j,0] = float(values[0])
+            corarra[j,1] = float(values[1])
+
+        return corarra[:,0],corarra[:,1]
+
+    def plot_rotation(self,start,coor,ax,dh=0,color='red',linestyle='-'):
+    
+    #add to stoptime 
+        stop = datetime.datetime.strptime(self.ofile[:-6],'%Y%m%d%H%M%S')
+        xs, ys = self.calc_poly_values(coor)
+    #calculate the mean position
+        stopx, stopy = solar_rotation.rot_hpc(float(xs[0])*u.arcsec,float(ys[0])*u.arcsec,start,stop)
+    #get rid of units
+        stopx, stopy = stopx.value, stopy.value
+    
+    
+        ax.plot(stopx,stopy,linestyle=linestyle,color=color)
+    #    ax.text(meanx,meany,'{0:6d}'.format(tid),color=color,fontsize=8)
+        return ax
+    
+    def plot_filament_track(self):
+
+    
+        dpi = 200
+        good, = np.where(self.dat['track_id'].values == self.tid)
+    
+    #get start time of track for filename
+#        ofname = '{0}_track{1:6d}'.format(dat['event_starttime'][good[0]],i).replace(' ','0').replace(':','_')
+        self.ofile = self.ifile.split('/')[-1].replace('fits.fz','png')
+        sun = pyfits.open(self.ifile)
+   
+        sundat = sun[1].data
+    
+    #create figure and add sun
+        fig, ax = plt.subplots(figsize=(7,7),dpi=dpi)
+
+        ax.imshow(sundat,cmap=plt.cm.gray)
+#        rs = plt.Circle((0.,0.),radius=1000.,color='gray',fill=False,linewidth=5,zorder=0)
+#        ax.add_patch(rs)
+    
+    #plot track polygons for given id
+        for j in good:
+            inc = 'red'
+            if self.dat['obs_observatory'].values[j] == 'HA2': inc='blue'
+            poly = plt.Polygon(loads(self.dat['hpc_bbox'].values[j]).exterior,color=inc,linewidth=0.5,fill=None)
+            ax.add_patch(poly)
+    #over plot rotation track
+            ax = self.plot_rotation(self.dat['event_starttime_dt'][j],self.dat['hpc_bbox'].values[j],ax,color='green')
+    
+    
+    #Setup plots
+        ticks = [-1000.,-500.,0.,500.,1000.]
+        lim = [-1200.,1200.]
+        ax.set_xlim(lim)
+        ax.set_ylim(lim)
+        ax.set_xticks(ticks)
+        ax.set_yticks(ticks)
+        ax.set_xlabel('Solar X [arcsec]')
+        ax.set_ylabel('Solar Y [arcsec]')
+    
+    #save fig
+        fig.savefig('track_plots/'+self.ofile,bbox_pad=.1,bbox_inches='tight',dpi=dpi)
+        fig.clear()
+        plt.close()
+    
