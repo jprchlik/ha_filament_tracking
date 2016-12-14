e4fa51b480d3dc9eb670c6c278fb7b6bd279ee8c
jprchlik <japrchlik@gmail.com>
2016-12-14 10:40:48 -0500

    Added help() function comments to grab_gong.py

--- obs_long_plot/grab_gong.py
+++ obs_long_plot/grab_gong.py
@@ -11,8 +11,11 @@ typdic['fits'] = 'haf'
 
 
 class grab_gong:
+    """The grab_gong class creates an object which can download files from GONG ftp archive. 
+       It takes just a start and endtime after initialization by main """
 
     def __init__(self,start,end,ftp,arcdir,nproc,skip,verbose):
+        """grab_gong class initialization which is called and set by main"""
         #get a list of directories
         self.dlev1 = ftp.nlst()
         self.ftp = ftp
@@ -29,6 +32,7 @@ class grab_gong:
         self.getfiles()
 
     def mkloc_arc(self):
+        """mkloc_arc creates an local file structure archive based on the available dates """
 #make local subdirectories
         for i in self.dlev1: 
             self.mkdirs(i)
@@ -40,6 +44,7 @@ class grab_gong:
 
 #Actually make the directories                 
     def mkdirs(self,adir):
+        """Test whether a directory archive already exits. If the directory exists then continue."""
         try:
             os.mkdir(adir)
         except:
@@ -49,6 +54,7 @@ class grab_gong:
 
 #get files in date range from ftp archive
     def getfiles(self):
+        """Retrieve file list and download files from the GONG archive. It requires not addition arguments than those set up by the object."""
     # list of days in range as datetime object
         dates = [ self.start +timedelta(n) for n in range(int ((self.end - self.start).days))]
     #create day file list
@@ -74,6 +80,7 @@ class grab_gong:
         
         
     def write_loc_files(self,fname):
+        """Check to see if a local version of the files already exists. Skip if it exits, but download if it does not."""
    
 #check to see if file exists 
         testfile = os.path.isfile(fname)
@@ -89,6 +96,14 @@ class grab_gong:
 
 def main(start,end,nproc=4,typ='fits',verbose=False,skip=100,
          larc='/Volumes/Pegasus/jprchlik/projects/ha_filaments/gong'):
+    """Main loop of program. It is written to send information to grab_gong class.
+       The program requires the start and end times be python datetime objects. 
+       The optional inputs are as follows: nproc is the number of processors to use while downloading (default = 4, deprecated),
+       typ is the type of file to download from the ftp server (default = 'fits'),
+       verbose is present but not well implemented,
+       skip is the file cadence to download (default = 100, so download only 1 file per 100),
+       larc is the local archive directory location (default = /Volumes/Pegasus/jprchlik/projects/ha_filaments/gong)""" 
+       
 
 #make sure archive dir has an ending /
     if larc[-1] != '/': larc = larc+'/'
@@ -103,7 +118,8 @@ def main(start,end,nproc=4,typ='fits',verbose=False,skip=100,
     try:
         os.chdir(arcdir)
     except:
-        print 'Folder not created in proper location. Check larc variable'
+        if verbose:
+            print 'Folder not created in proper location. Check larc variable'
         return 1
 
 #connect to gong ftp archive
