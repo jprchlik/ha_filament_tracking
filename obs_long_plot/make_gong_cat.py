0e8cbb5f3146fd5b5f976060333fd5b9be792d80
jprchlik <japrchlik@gmail.com>
2017-05-12 10:08:42 -0400

    turned make gong catalog into a callable function

--- obs_long_plot/make_gong_cat.py
+++ obs_long_plot/make_gong_cat.py
@@ -13,6 +13,7 @@ def get_best_track(start,end,local=False):
     return flist
     
 def create_images(i):
+    global dat
     out = halpha_plot(dat,i,pdir)
     out.plot_filament_track()
     
@@ -24,64 +25,57 @@ def create_images(i):
 #start = datetime.strptime('2013/01/00T00:00:00',fmt)
 #end = datetime.strptime('2013/01/31T23:59:59',fmt)
 
-infile = '../init_data/FITracked_3yr.pic'
-infile = 'concatentated_per_shape_3yr_file.pic'
-
-pickled = os.path.isfile(infile)
-if pickled:#use pickle file if it already exits
-    dat = pd.read_pickle(infile)
-else: #create pickle file if doesnt exist
-    infile = '../init_data/FITracked_3yr.txt'
-    #dat = ascii.read('../init_data/FITracked_3yr.txt',delimiter='\t',guess=False)
-    dat = pd.read_csv(infile,delimiter='\t')
-    #add variables like datetime and average position to dat
-    dat = ap.add_props(dat).dat
-    dat.to_pickle('../init_data/FITracked_3yr.pic')
-
-dfmt = '%Y-%m-%dT%H:%M:%S'
-#set up plot directory
-sdir = os.getcwd()
-pdir = sdir+'/track_plots/'
-
-try:
-    os.mkdir(pdir)
-except:
-    print 'Directory {0} already exists'.format(pdir)
-try:
-    os.mkdir(pdir+'symlinks')
-except:
-    print 'Directory {0} already exists'.format(pdir+'symlinks')
-
-
-nproc = 8
-#do in parallel all good tracks
-#pool =Pool(processes=nproc)
-#out = pool.map(get_best_track,goodtracks)
-#pool.close()
-
-
-start = '2012-01-01T00:00:00'
-end   = '2014-12-01T00:00:00'
-
-
-ostart = datetime.strptime(start,dfmt)
-oend   = datetime.strptime(end,dfmt)
-
-rdir = os.getcwd() #return to current working directory before making movie
-
-print 'STARTING DOWNLOAD'
-flist = get_best_track(ostart,oend,local=True)
-
-os.chdir(rdir)
-#do in parallel 
-#for mm in flist: create_images(mm)
-pool =Pool(processes=nproc)
-out = pool.map(create_images,flist)
-pool.close()
-
-
-os.chdir(rdir)
-#create movie from image files
-imov = make_movie.create_movie(w0=2048,h0=2048,nproc=4,outmov='halpha_filament_movie_per_shape.mp4')
-imov.create_movie()
+def main(infile,outmov,start='2012-01-01T00:00:00',end='2014-12-01T00:00:00',dfmt = '%Y-%m-%dT%H:%M:%S'):
+    pickled = os.path.isfile(infile)
+    if pickled:#use pickle file if it already exits
+        dat = pd.read_pickle(infile)
+    else: #create pickle file if doesnt exist
+        infile = '../init_data/FITracked_3yr.txt'
+        #dat = ascii.read('../init_data/FITracked_3yr.txt',delimiter='\t',guess=False)
+        dat = pd.read_csv(infile,delimiter='\t')
+        #add variables like datetime and average position to dat
+        dat = ap.add_props(dat).dat
+        dat.to_pickle('../init_data/FITracked_3yr.pic')
+    
+    
+    #set up plot directory
+    sdir = os.getcwd()
+    pdir = sdir+'/track_plots/'
+    
+    try:
+        os.mkdir(pdir)
+    except:
+        print 'Directory {0} already exists'.format(pdir)
+    try:
+        os.mkdir(pdir+'symlinks')
+    except:
+        print 'Directory {0} already exists'.format(pdir+'symlinks')
+    
+    
+    nproc = 8
+    #do in parallel all good tracks
+    #pool =Pool(processes=nproc)
+    #out = pool.map(get_best_track,goodtracks)
+    #pool.close()
+    
+    ostart = datetime.strptime(start,dfmt)
+    oend   = datetime.strptime(end,dfmt)
+    
+    rdir = os.getcwd() #return to current working directory before making movie
+    
+    print 'STARTING DOWNLOAD'
+    flist = get_best_track(ostart,oend,local=True)
+    
+    os.chdir(rdir)
+    #do in parallel 
+    #for mm in flist: create_images(mm)
+    pool =Pool(processes=nproc)
+    out = pool.map(create_images,flist)
+    pool.close()
+    
+    
+    os.chdir(rdir)
+    #create movie from image files
+    imov = make_movie.create_movie(w0=2048,h0=2048,nproc=4,outmov=outmov)
+    imov.create_movie()
 
